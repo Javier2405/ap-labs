5c00335aecdcf3f6729197c91e84bf65bb3ae047
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2020-02-22 11:44:18 -0600

    Solved word reversed
    Signed-off-by: Victor Javier Aguayo Mendoza <a01229260@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
+    	char c1;
+	int cont1 = 0;
+	char word[50];
+	printf("Enter a word: ");
+	while( (c1 = getchar()) != EOF){
+		if(c1 == '\n'){
+			cont1--;
+			while(cont1>=0){
+				printf("%c",word[cont1--]);
+			}
+			printf("\nEnter a word: ");
+			cont1=0;
+		}else{ 
+			word[cont1++]=c1;
+		}
+	}
     return 0;
 }
