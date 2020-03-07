e3521f653b54a313f341752144bfefd009dfabe1
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2020-03-06 20:45:52 -0600

    Solved slices and maps
    Signed-off-by: Victor Javier Aguayo Mendoza <a01229260@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	total := make(map[string]int)
+	words := strings.Fields(s)
+	for i := range words {
+		total[words[i]] = total[words[i]] + 1
+	}
+	return total
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,19 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var a[][]uint8
+	a = make([][]uint8, dy)
+	for i:= 0; i < dy; i++ {
+		a[i] = make([]uint8, dx)
+		for j:=0; j < dx; j++ {
+			a[i][j] = uint8((1500*j^i)^(j*i/(3*(j^i)+1)))
+		}
+	}
+	return a
+}
+
 func main() {
+	pic.Show(Pic)
 }
