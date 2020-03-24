34bb89ac7d4336e354a4bcb0c58db9c11da4d15e
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2020-03-24 10:04:28 -0600

    Solved mycat lab
    Signed-off-by: Victor Javier Aguayo Mendoza <a01229260@itesm.mx>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>  
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifp, int ofp)
 {
-    int c;
-
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
+    char line[200];
+    int c; //lenght of line in bytes
+    while ((c = read(ifp, line, 200)) > 0){
+        if(write(ofp, line, c)!= c){
+            write(2, "Error while writting\n",strlen("Error while writting\n"));
+        }
+    }
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
     char *prog = argv[0];   /* program name for errors */
-
-    if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
-    else
+    if (argc == 1){  /* no args; copy standard input */
+        filecopy(0, 1);
+    }
+    else{
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
+            if ((fp = open(*++argv, O_RDONLY)) == -1) {
+                write(2, prog, strlen(prog));
+                write(2, ": can't open",strlen(": can't open"));
+                write(2, *argv, strlen(*argv));
+                write(2, "\n",strlen("\n"));
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fp, 1);
+                close(fp);
             }
-
+    }
     if (ferror(stdout)) {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
+        write(2, prog, strlen(prog));
+        write(2, ": error writing\n",strlen(": error writing\n"));
         return 2;
     }
-
     return 0;
 }
